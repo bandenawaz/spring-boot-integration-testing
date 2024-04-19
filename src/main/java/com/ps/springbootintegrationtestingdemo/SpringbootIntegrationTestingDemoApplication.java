11763cfb8f817ca323e05d265056c5f098b2a82d
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2024-04-19 08:58:34 +0530

    initial setup

---
+++ src/main/java/com/ps/springbootintegrationtestingdemo/SpringbootIntegrationTestingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ps.springbootintegrationtestingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntegrationTestingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIntegrationTestingDemoApplication.class, args);
+	}
+
+}
